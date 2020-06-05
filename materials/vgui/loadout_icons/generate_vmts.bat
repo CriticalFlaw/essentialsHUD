4f93a5b3ddae6d57ff8ddfed815e1983c127fe32
CriticalFlaw <nik_101@live.co.uk>
2020-06-05 16:37:30 -0400

    Added Weapon Icons
    - Added missing loadout icons, including the PASS Time jack.
    - Updated the batch scripts, for my own sake.

--- materials/vgui/loadout_icons/generate_vmts.bat
+++ materials/vgui/loadout_icons/generate_vmts.bat
@@ -1,21 +1,35 @@
 @echo off
 
-echo Deleting previous vmts...
+echo Deleting previous VMTs...
 DEL *.vmt
 
+echo Generating Red VMTs...
 for %%X IN (*.vtf) DO (
 
-  echo "UnlitGeneric"| tee %%~nX_red.vmt > %%~nX_blue.vmt
-  echo {| tee -a %%~nX_red.vmt >> %%~nX_blue.vmt
-  echo     $basetexture vgui/loadout_icons/%%~nX| tee -a %%~nX_red.vmt >> %%~nX_blue.vmt
-  echo     $translucent 1| tee -a %%~nX_red.vmt >> %%~nX_blue.vmt
-  echo     $vertexcolor 1| tee -a %%~nX_red.vmt >> %%~nX_blue.vmt
-  echo     $vertexalpha 1| tee -a %%~nX_red.vmt >> %%~nX_blue.vmt
+  echo "UnlitGeneric">> %%~nX_red.vmt
+  echo {>> %%~nX_red.vmt
+  echo     $basetexture vgui/loadout_icons/%%~nX>> %%~nX_red.vmt
+  echo     $translucent 1 >> %%~nX_red.vmt
+  echo     $vertexcolor 1 >> %%~nX_red.vmt
+  echo     $vertexalpha 1 >> %%~nX_red.vmt
 
   REM Flip the red team
   echo     $basetexturetransform "center .5 .5 scale -1 1 rotate 0 translate 0 0">> %%~nX_red.vmt
 
-  echo }| tee -a %%~nX_red.vmt >> %%~nX_blue.vmt
-
-  echo     %%~nX
+  echo }>> %%~nX_red.vmt
 )
+
+echo Generating Blue VMTs...
+for %%X IN (*.vtf) DO (
+
+  echo "UnlitGeneric">> %%~nX_blue.vmt
+  echo {>> %%~nX_blue.vmt
+  echo     $basetexture vgui/loadout_icons/%%~nX>> %%~nX_blue.vmt
+  echo     $translucent 1 >> %%~nX_blue.vmt
+  echo     $vertexcolor 1 >> %%~nX_blue.vmt
+  echo     $vertexalpha 1 >> %%~nX_blue.vmt
+
+  echo }>> %%~nX_blue.vmt
+)
+
+echo Done!
